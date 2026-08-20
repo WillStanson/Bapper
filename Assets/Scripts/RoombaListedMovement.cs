fc4b79100dce1c6d5c5532065413d76b0992b5f7
William <william.stewart993@gmail.com>
2026-08-20 16:48:47 +0200

    FikserRoombaMovement

---
+++ Assets/Scripts/RoombaListedMovement.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RoombaListedMovement : MonoBehaviour
+{
+    [SerializeField] private List<Transform> points;
+    [SerializeField] private float Speed = 5;
+    private Transform CurrentPoint;
+    private Vector3 CurrentDir;
+    private Rigidbody rb;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CurrentPoint = points[1];
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardMovement(1);
+    }
+
+    void ForwardMovement(int movementDirection)
+    {
+        float movementAmount = Speed * movementDirection * Time.deltaTime;
+
+        rb.linearVelocity = transform.forward * Speed * movementDirection;
+
+    }
+}
--- Assets/Scripts/RoombaMovement.cs
+++ Assets/Scripts/RoombaMovement.cs
@@ -2,67 +2,75 @@ using UnityEngine;
 
 public class RoombaMovement : MonoBehaviour
 {
-
     [SerializeField] private float minXPos;
     [SerializeField] private float maxXPos;
-    [SerializeField] private float minYPos;
-    [SerializeField] private float maxYPos;
+    [SerializeField] private float minZPos;
+    [SerializeField] private float maxZPos;
     [SerializeField] private float patrolSpeed;
     [SerializeField] private bool isMovingDown;
     [SerializeField] private bool isMovingUp;
     [SerializeField] private bool isMovingRight;
     [SerializeField] private bool isMovingLeft;
-    private Vector2 moveDir;
+    private Vector3 moveDir;
+    private Rigidbody rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         if (isMovingLeft)
         {
             // sets move direction to left
             moveDir.x = -patrolSpeed;
+            moveDir.z = 0;
 
-            // switches to right if it moves to/past the defined minimum x position
+            // 
             if (transform.position.x <= minXPos)
-                moveDir.x = 0;
                 isMovingLeft = false;
                 isMovingUp = true;
         }
         else if (isMovingUp)
         {
             // sets move direction to up
-            moveDir.y = patrolSpeed;
+            moveDir.z = patrolSpeed;
+            moveDir.x = 0;
 
-            // switches to left if it moves to/past the defined maximum x position
-            if (transform.position.x >= maxXPos)
-                moveDir.y = 0;
-                isMovingRight = true;
+            // 
+            if (transform.position.z >= maxZPos)
                 isMovingUp = false;
+                isMovingRight = true;
+                
         }
         else if (isMovingRight)
         {
             // sets move direction to right
             moveDir.x = patrolSpeed;
+            moveDir.z = 0;
 
-            // switches to right if it moves to/past the defined minimum x position
-            if (transform.position.x <= minXPos)
-                moveDir.x = 0;
-                isMovingLeft = false;
+            // 
+            if (transform.position.x >= maxXPos)
+                isMovingRight = false;
                 isMovingDown = true;
         }
         else if (isMovingDown)
         {
             // sets move direction to down
-            moveDir.y = -patrolSpeed;
+            moveDir.z = -patrolSpeed;
+            moveDir.x = 0;
 
-            // switches to left if it moves to/past the defined maximum x position
-            if (transform.position.x >= maxXPos)
-                moveDir.y = 0;
-                isMovingLeft = true;
+            // 
+            if (transform.position.z <= minZPos)
                 isMovingDown = false;
+                isMovingLeft = true;
+                
         }
         else
         {
             print("roomba movement script broke");
         }
+
+        rb.position += moveDir * Time.deltaTime;
     }
 }
